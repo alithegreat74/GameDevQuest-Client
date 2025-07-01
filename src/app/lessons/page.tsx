e077e2c98e53db7a32eeba3922f5d0e6453df342
mamad_mmp <mamadmalekpour1380@gmail.com>
2025-07-01 12:40:11 +0330

    #6 lesson card updated

--- src/app/lessons/page.tsx
+++ src/app/lessons/page.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import SeparatorLine from "../components/SeparatorLine";
 
 import "./lessons.css";
+import Image from "next/image";
 
 const Lessons = () => {
   return (
@@ -24,6 +25,48 @@ const Lessons = () => {
           <a href="#">In-Progress</a>
           <a href="#">Completed</a>
         </nav>
+        <section id="lessons_container" className="flex flex_column">
+          <div className="lesson_card">
+            <Image
+              src={"/Lesson.jpg"}
+              alt={"Summoning Sprite with Unity"}
+              width={300}
+              height={200}
+            />
+            <div className="lesson_details">
+              <div className="lesson_tags flex gap_16">
+                <span className="lesson_tag">
+                  <Image
+                    src={"/difficulty1.svg"}
+                    alt={"difficulty1"}
+                    width={20}
+                    height={20}
+                  />
+                  Easy
+                </span>
+                <span className="lesson_tag">
+                  <Image
+                    src={"/difficulty2.svg"}
+                    alt={"difficulty1"}
+                    width={20}
+                    height={20}
+                  />
+                  Medium
+                </span>
+              </div>
+              <h2>Summoning Sprites with Unity</h2>
+              <div className="lesson_progress flex">
+                <progress value={40} max={100}></progress> 40% Completed
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit accusamus quo quam, quibusdam dolores quae vero
+                eveniet iste, voluptas sed laboriosam alias exercitationem!
+                Neque cum facere mollitia nemo, ducimus natus.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
