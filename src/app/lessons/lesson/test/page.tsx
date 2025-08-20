4ff71af8ec288ef25ac85a410b85fadfcb8ea19c
mamad_mmp <mamadmalekpour1380@gmail.com>
2025-08-20 15:42:05 +0330

    #18 test page implementation done

--- src/app/lessons/lesson/test/page.tsx
+++ src/app/lessons/lesson/test/page.tsx
@@ -29,12 +29,47 @@ const Lesson = () => {
               <h1 className="text_gradient">Summoning Sprites with Unity</h1>
             </hgroup>
             <div className="flex_center gap_8">
-              <span className="font_16_14"><b>4</b></span>
-              <progress value={4} max={10}></progress>
+              <span className="font_16_14">
+                <b>4</b>
+              </span>
+              <progress id="test_progress" value={4} max={10}></progress>
               <span className="font_16_14">10</span>
             </div>
           </div>
         </section>
+        <article id="test_section">
+          <form action="">
+            <p id="question" className="text_read">
+              <span>1.</span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              inventore quam culpa magni illo architecto dolorem itaque nam
+              ipsam odio eligendi perferendis molestiae, quod nobis voluptatem,
+              dolor amet magnam et.
+            </p>
+            <div id="answers" className="flex flex_column">
+              <label>
+                <input type="radio" name="question1" value="a" />
+                <span>Answer A</span>
+              </label>
+              <label>
+                <input type="radio" name="question1" value="b" />
+                <span>Answer B</span>
+              </label>
+              <label>
+                <input type="radio" name="question1" value="c" />
+                <span>Answer C</span>
+              </label>
+              <label>
+                <input type="radio" name="question1" value="d" />
+                <span>Answer D</span>
+              </label>
+            </div>
+          </form>
+          <div id="test_buttons" className="flex_center">
+            <button className="main_btn">BACK</button>
+            <button className="main_btn">NEXT</button>
+          </div>
+        </article>
       </main>
     </>
   );
