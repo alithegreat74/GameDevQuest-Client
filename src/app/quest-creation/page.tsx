f847147866511211832f172f4b97c08e33cccb31
Mamad Malekpour <mamadmalekpour1380@gmail.com>
2025-09-14 12:01:17 +0330

    #46 quest creation page implementation done

---
+++ src/app/quest-creation/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from "react";
+import Header from "../components/Header";
+import SeparatorLine from "../components/SeparatorLine";
+import MDEditor from "@uiw/react-md-editor";
+
+import "./quest-creation.css";
+import TextInput from "../components/TextInput";
+import Footer from "../components/Footer";
+
+const Lesson = () => {
+  const [value, setValue] = React.useState<string>("");
+
+  const handleEditorChange: React.Dispatch<React.SetStateAction<string>> = (
+    newValue
+  ) => {
+    setValue(newValue || "");
+  };
+
+  return (
+    <>
+      <Header />
+      <SeparatorLine text={"Quest Creation"} />
+      <main className="max_width">
+        <hgroup>
+          <h1 className="text_gradient">Forge Quest</h1>
+          <p>
+            Design a challenge, set the rewards, and release it into the world
+            for other adventurers to undertake.
+          </p>
+        </hgroup>
+        <section id="quest_creation">
+          <TextInput
+            label={"Quest Title"}
+            placeholder={"Title..."}
+            type={"text"}
+            inputName={"quest-title"}
+          />
+          <div id="mdeditor" className="flex flex_column gap_8">
+            <label htmlFor="" className="text_gradient">
+              Quest Body
+            </label>
+            <MDEditor
+              value={value}
+              onChange={handleEditorChange}
+              height={400}
+            />
+          </div>
+          <div id="submit_quest_button">
+            <button className="main_btn">Submit</button>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Lesson;
